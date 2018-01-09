499405636188eace77c69976e8ec163c43c10920
航 <2719091861@qq.com>
2018-01-09 21:40:38 +0800

    12306
    12306

---
+++ 12306/user_12306.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2017/8/30 19:15
+# @Author  : 强子
+# @File    : user_12306.py
+# @Software: PyCharm
+user = 'cai345408904'
+password = 'cai15928021496'
